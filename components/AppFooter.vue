15aec14903cd7b5976892a4b3bc8d5d2f9a78e57
jeric realubit <jericrealubit@gmail.com>
2025-05-20 11:55:10 +1200

    add hover cursor pointer to theme toggle

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-4 border-t dark:border-gray-800">
     <div class="flex items-center justify-between">
-      <ThemeToggle />
+      <ThemeToggle class="hover:cursor-pointer" />
       <div class="text-sm text-gray-500">
         © {{ new Date().getFullYear() }} WeatherApp - Powered by
         <NuxtLink to="https://www.weatherapi.com/" title="Free Weather API"
